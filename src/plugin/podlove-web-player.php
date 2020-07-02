c2bf02916a54dc57283e08fa109b670c9cc375d4
Alexander Heimbuch <github@heimbu.ch>
2020-07-02 19:18:16 +0200

    chore(multi-site): fix plugin base path detection

--- src/plugin/podlove-web-player.php
+++ src/plugin/podlove-web-player.php
@@ -31,7 +31,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 define( 'PODLOVE_WEB_PLAYER_VERSION', '5.2.2' );
-define( 'PODLOVE_WEB_PLAYER_PATH', plugins_url( basename( __DIR__ ) ) );
+define( 'PODLOVE_WEB_PLAYER_PATH', plugins_url( '.', __FILE__ ) );
 
 /**
  * The code that runs during plugin activation.
