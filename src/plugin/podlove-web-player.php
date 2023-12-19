8da8f1b7a8fe988f58240fa8ab87cc0cef0631a1
Alexander Heimbuch <github@heimbu.ch>
2023-12-19 20:12:35 +0100

    Release 5.7.2

--- src/plugin/podlove-web-player.php
+++ src/plugin/podlove-web-player.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Podlove Web Player
  * Plugin URI:        https://docs.podlove.org/podlove-web-player/
  * Description:       Audio First Podcast Web Player
- * Version:           5.7.1
+ * Version:           5.7.2
  * Author:            Podlove
  * Author URI:        http://podlove.org
  * License:           MIT
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'PODLOVE_WEB_PLAYER_VERSION', '5.7.1' );
+define( 'PODLOVE_WEB_PLAYER_VERSION', '5.7.2' );
 define( 'PODLOVE_WEB_PLAYER_PATH', plugins_url( '', __FILE__ ) );
 
 /**
